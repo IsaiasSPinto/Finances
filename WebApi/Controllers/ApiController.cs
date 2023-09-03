7463839642594ae597b30b5c6ed70c28c3702231
IsaiasSPinto <isaiascxs10@gmail.com>
2023-09-03 11:29:21 -0300

    api controllers

---
+++ WebApi/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ApiController : ControllerBase
+{
+    protected readonly IMediator _mediator;
+    public ApiController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class UserController : ApiController
+{
+    public UserController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpGet] 
+    public async Task<IActionResult> Get()
+    {
+
+        return Ok();
+    }
+
+}
